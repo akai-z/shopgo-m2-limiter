4c398a592f8ef5a42dd64d5b8cd8c73d1a43142a
Ammar <ammar@shopgo.me>
2016-02-20 00:13:04 +0200

    Add SNS notification event observer

---
+++ Observer/SnsNotification.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright © 2015 ShopGo. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace ShopGo\Limiter\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * Sns notification observer
+ */
+class SnsNotification implements ObserverInterface
+{
+    /**
+     * Limiter code
+     */
+    const CODE = 'limiter';
+
+    /**
+     * Set SKU limit action
+     */
+    const ACTION_SET_SKU_LIMIT = 'set_sku_limit';
+
+    /**
+     * Notifier model
+     *
+     * @var \ShopGo\Limiter\Model\Sku
+     */
+    protected $_skuLimiter;
+
+    /**
+     * @param \ShopGo\Limiter\Model\Sku $skuLimiter
+     */
+    public function __construct(\ShopGo\Limiter\Model\Sku $skuLimiter)
+    {
+        $this->_skuLimiter = $skuLimiter;
+    }
+
+    /**
+     * Handle SNS notifications
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $notification = $observer->getEvent()->getData('notification');
+
+        if ($notification['module'] != self::CODE) {
+            return false;
+        }
+
+        switch ($notification['action']) {
+            case self::ACTION_SET_SKU_LIMIT:
+                $this->_skuLimiter->setLimit(
+                    $notification['arguments']['sku_limit']
+                );
+                break;
+        }
+    }
+}
